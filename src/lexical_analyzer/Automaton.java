a088d7a4d9c49e8c5a71977ea4305c4805b0e36f
Pedro Correa <pedro8correa@gmail.com>
2020-09-28 20:56:14 -0300

    feat: finished project

--- src/lexical_analyzer/Automaton.java
+++ src/lexical_analyzer/Automaton.java
@@ -9,7 +9,7 @@ public class Automaton {
 
 	private String[] tipos = { "boolean", "int", "float" };
 	private String[] ignoreLine = { "/*", "*/" };
-	private String[] points = { "{", "}", ">", "<", "(", ")" };
+	private String[] points = { "{", "}", ">", "<", "(", ")", ";" };
 	private String[] reservedWords = { "se", "senao", "imprimi" };
 	private List<String> palavrasReservadas = new ArrayList<>();
 
@@ -21,9 +21,25 @@ public class Automaton {
 		this.palavrasReservadas.addAll(Arrays.asList(reservedWords));
 
 		if(!line.equals("")){
-			String[] words = line.split(" ");
+			String[] words = line.replace(";", " ;").split(" ");
+
+      List<String> commentary = new ArrayList<>();
+      Boolean hasCommentary = false;
 
 			for (String word : words) {
+
+        if (hasCommentary || word.equals(ignoreLine[0])) {
+          hasCommentary = true;
+          commentary.add(word);
+          if (word.equals(ignoreLine[1])) {
+            hasCommentary = false;
+            Output.setCommentIdentifier(String.join(" ", commentary));
+            commentary = new ArrayList<>();
+          }
+          continue;
+        }
+
+
 				if (palavrasReservadas.contains(word)) {
 					this.treatReservedWord(word);
 				} else if (isNumber(word)) {
@@ -31,13 +47,8 @@ public class Automaton {
 				} else {
 					Output.setVariableIdentifier(word);
 				}
-
-				// if(error){
-				// 	Error.addError(lineNumber);
-				// 	break;
-				// }
 			}
-		}else{
+		} else {
 			Error.addError(lineNumber);
 		}
 	}
@@ -49,8 +60,6 @@ public class Automaton {
 			Output.setPunctuation(word);
 		} else if (Arrays.asList(reservedWords).contains(word)) {
 			Output.setReservedWord(word);
-		} else if (Arrays.asList(ignoreLine).contains(word)) {
-			Output.setCommentIdentifier(word);
 		}
 	}
 	
